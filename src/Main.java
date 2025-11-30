aecda10a4b189a60f8545655234830debca9ce58
Chtsone <124277087+Chtsone@users.noreply.github.com>
2025-11-30 14:13:25 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,25 @@ public class Main
             {
                 Scanner scanner = new Scanner(System.in);
 
-//                Task1 task1 = new Task1();
-//                task1.printAllTest(new double[]{30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675});
-//
-//                Task2 task2 = new Task2();
-//                task2.testPowPrint();
-//
-//                Task3 task3 = new Task3();
-//                System.out.print("Введите количество чисел n: ");
-//                task3.printSignSeriesInfo(task3.creatNumArray(scanner.nextInt()));
-//                scanner.close();
-//
-//                Task4 month1 = new Task4(LocalDate.of(2023, 11, 12));
-//                month1.printAllInfo();
-//                Task4 month2 = new Task4();
-//                month2.printAllInfo();
+                Task1 task1 = new Task1();
+                task1.printAllTest(new double[]{30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675});
 
-                //DemoRunner.runCountdown(args, scanner); // Task5
-                //DemoRunner.runArrayStatistics();        // Task6
-                //DemoRunner.runPriorityStack();          // Task7
+                Task2 task2 = new Task2();
+                task2.testPowPrint();
+
+                Task3 task3 = new Task3();
+                System.out.print("Введите количество чисел n: ");
+                task3.printSignSeriesInfo(task3.creatNumArray(scanner.nextInt()));
+                scanner.close();
+
+                Task4 month1 = new Task4(LocalDate.of(2023, 11, 12));
+                month1.printAllInfo();
+                Task4 month2 = new Task4();
+                month2.printAllInfo();
+
+                DemoRunner.runCountdown(args, scanner); // Task5
+                DemoRunner.runArrayStatistics();        // Task6
+                DemoRunner.runPriorityStack();          // Task7
 
             }
 
